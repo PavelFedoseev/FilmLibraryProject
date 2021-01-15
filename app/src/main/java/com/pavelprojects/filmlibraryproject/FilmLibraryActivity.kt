0be90459d23e198c5bf479eb722a761619383c8f
Fedoseev Pavel <fedoseev.p.m@gmail.com>
2021-01-16 01:28:20 +0300

    First master Commit

---
+++ app/src/main/java/com/pavelprojects/filmlibraryproject/FilmLibraryActivity.kt
@@ -0,0 +1,11 @@
+package com.pavelprojects.filmlibraryproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FilmLibraryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_filmlibrary)
+    }
+}
